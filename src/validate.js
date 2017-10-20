ad158a919606776d79aa40859312afe624bfbd46
Soeren Vibjerg <vibjerg@gmail.com>
2017-10-20 11:06:29 +0200

    Add method for validating responses

--- src/validate.js
+++ src/validate.js
@@ -1,9 +1,14 @@
-import {validate} from 'jsonschema';
+import {Validator, validate} from 'jsonschema';
+var v = new Validator();
+import {getSpecification, getSchemaDefinition} from './swaggerFromSpec';
+const swagger = getSpecification('definitions-api');
+v.addSchema(swagger, '/');
+
 import fs from 'fs';
 import yaml from 'js-yaml';
 
 let schemas;
-
+let responseSchemas; 
 function getSchema(name) {
   if (!schemas) {
     schemas = {};
@@ -24,7 +29,31 @@ function getSchema(name) {
   return schemas[name] || {};
 }
 
-export default (name, params) => {
-  const schema = getSchema(name);
+function getResponseSchema(name) {
+  if (!responseSchemas) {
+    responseSchemas = {};
+    const fullSpec = yaml.safeLoad(fs.readFileSync(__dirname + '/../doc/spec.yaml', 'utf-8'));
+    const keys = Object.keys(fullSpec.api);
+
+    keys.forEach((key) => {
+      responseSchemas[key] = {};
+      const spec = fullSpec.api[key];
+      if (spec.response && spec.response.$ref) {
+        responseSchemas[key] = spec.response.$ref;
+      }
+    }); 
+  }
+  return responseSchemas[name] || {};
+
+}
+
+export function validateRequest (name, params) {
+  const schema = getSchema(name); 
   return validate(params, schema).errors;
-};
+}
+
+export function validateResponse(name, response) {
+  const schema = getResponseSchema(name);
+  return v.validate(response, schema).errors;
+}
+
