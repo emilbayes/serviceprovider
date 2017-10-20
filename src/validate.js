c15fb5f0cb66e57aeae99ab523319e00116dcf06
Soeren Vibjerg <vibjerg@gmail.com>
2017-10-20 14:01:10 +0200

    cleanup and add inline documentation for validate.js

--- src/validate.js
+++ src/validate.js
@@ -1,57 +1,63 @@
 import {Validator, validate} from 'jsonschema';
-var v = new Validator();
 import loadSwagger from './swaggerFromSpec';
-const swagger = loadSwagger();
-v.addSchema(swagger, '/');
-
 import fs from 'fs';
 import yaml from 'js-yaml';
 
-let schemas;
-let responseSchemas; 
-function getSchema(name) {
-  if (!schemas) {
-    schemas = {};
-    const fullSpec = yaml.safeLoad(fs.readFileSync(__dirname + '/../doc/spec.yaml', 'utf-8'));
-    const keys = Object.keys(fullSpec.api);
+// Setup validator
+var v = new Validator();
+const swagger = loadSwagger();
+v.addSchema(swagger, '/');
 
-    keys.forEach((key) => {
-      const spec = fullSpec.api[key];
-      schemas[key] = {
-        type: 'object',
-        required: spec.required,
-        additionalProperties: false,
-        properties: Object.assign({}, fullSpec.defaultProperties, spec.properties)
-      };
-    });
+// Placeholders for schemas
+const fullSpec = yaml.safeLoad(fs.readFileSync(__dirname + '/../doc/spec.yaml', 'utf-8'));
+
+/**
+ * Get JSON Schema for named request.
+ * 
+ * @param {String} name 
+ */
+function getRequestSchema(name) {
+  const spec = fullSpec.api[name];
+  if (spec) {
+    return {
+      type: 'object',
+      required: spec.required,
+      additionalProperties: false,
+      properties: Object.assign({}, fullSpec.defaultProperties, spec.properties)
+    };
   }
-
-  return schemas[name] || {};
+  return {};
 }
 
+/**
+ * Get named JSON Schema for response.
+ * @param {String} name 
+ */
 function getResponseSchema(name) {
-  if (!responseSchemas) {
-    responseSchemas = {};
-    const fullSpec = yaml.safeLoad(fs.readFileSync(__dirname + '/../doc/spec.yaml', 'utf-8'));
-    const keys = Object.keys(fullSpec.api);
-
-    keys.forEach((key) => {
-      responseSchemas[key] = {};
-      const spec = fullSpec.api[key];
-      if (spec.response && spec.response.$ref) {
-        responseSchemas[key] = spec.response.$ref;
-      }
-    }); 
+  const spec = fullSpec.api[name];
+  if (spec.response && spec.response) {
+    return spec.response;
   }
-  return responseSchemas[name] || {};
-
+  return {};
 }
 
+/**
+ * Validate Request against Schema.
+ * 
+ * @param {String} name 
+ * @param {Object} params 
+ */
 export function validateRequest (name, params) {
-  const schema = getSchema(name); 
+  const schema = getRequestSchema(name); 
   return validate(params, schema).errors;
 }
 
+/**
+ * Validate response against JSON Schema.
+ * 
+ * @param {String} name 
+ * @param {Object} response 
+ */
 export function validateResponse(name, response) {
   const schema = getResponseSchema(name);
   return v.validate(response, schema).errors;
